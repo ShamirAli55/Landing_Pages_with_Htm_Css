515154657a9679c981ad6dca2faebcd01d98f999
ShamirAli55 <shamirali9779@gmail.com>
2025-07-26 02:08:15 -0700

    Gsap Animation Added

--- 05/script.js
+++ 05/script.js
@@ -1,25 +1,39 @@
-const tl = gsap.timeline({ 
-    repeat: -1, 
-    repeatDelay: 0 
-},0);
+const tl = gsap.timeline(
+  {
+    repeat: -1,
+    repeatDelay: 0,
+  },
+  0
+);
 
+tl.to(
+  ".img-container",
+  {
+    ease: Expo.easeInOut,
+    width: "100%",
+    duration: 2,
+    stagger: 2,
+  },
+  0
+);
 
-tl.to('.img-container',{
-    ease : Expo.easeInOut,
-    width : "100%",
-    duration:2,
-    stagger : 2
-},0);
+tl.to(
+  ".text h1",
+  {
+    ease: Expo.easeInOut,
+    stagger: 2.3,
+    top: 0,
+  },
+  0
+);
 
-tl.to(".text h1",{
-    ease : Expo.easeInOut,
-    stagger :2.3,
-    top:0,
-},0);
-
-tl.to(".text h1",{
-    delay :2,
-    ease : Expo.easeInOut,
-    stagger : 2,
-    top:"-100%"
-},0);
+tl.to(
+  ".text h1",
+  {
+    delay: 2,
+    ease: Expo.easeInOut,
+    stagger: 2,
+    top: "-100%",
+  },
+  0
+);
